b060ae0dbe93a18e44192baa1a6696caca5ffd28
Lenny Peters <mr.lennypeters@gmail.com>
2016-01-21 21:47:08 -0800

    Master - localstorage and sessionstorage rocking

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,25 @@ var angular;
 angular.module('myApp', [
     'ui.router',
     'ngStorage'
-]);
+])
+
+.controller('mainController',function ($scope, $localStorage, $sessionStorage){
+	$scope.save = function () {
+		$localStorage.message = "Hello World";
+	};
+
+	$scope.load = function () {
+		$localStorage.data = $localStorage.message;
+	};
+
+	$scope.sessionsave = function () {
+		$sessionStorage.message = "Hello World";
+	};
+
+	$scope.sessionload = function () {
+		$sessionStorage.data = $sessionStorage.message;
+	};
+});
 
 var describe, it, expect;
 
--- src/app/app.js
+++ src/app/app.js
@@ -5,4 +5,22 @@ var angular;
 angular.module('myApp', [
     'ui.router',
     'ngStorage'
-]);
+])
+
+.controller('mainController',function ($scope, $localStorage, $sessionStorage){
+	$scope.save = function () {
+		$localStorage.message = "Hello World";
+	};
+
+	$scope.load = function () {
+		$localStorage.data = $localStorage.message;
+	};
+
+	$scope.sessionsave = function () {
+		$sessionStorage.message = "Hello World";
+	};
+
+	$scope.sessionload = function () {
+		$sessionStorage.data = $sessionStorage.message;
+	};
+});
