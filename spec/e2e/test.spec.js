7905246118f451e0c3fec1333adefeb11a0647e0
Lenny Peters <lpeters@Lenny-Peters-MacBook-Pro.local>
2016-03-30 13:01:42 -0700

    Master - changing the test.spec.js file

--- spec/e2e/test.spec.js
+++ spec/e2e/test.spec.js
@@ -1,10 +1,11 @@
+//test for protractor to see if its working
 describe('truthy test', function () {
 	it('should be truthy', function () {
 		expect(true).toBe(true);
 	});
 });
 
-//test e2e for the protractor
+//test e2e for the protractor simple example
 describe('Protractor Demo App', function() {
   it('should have a title', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
@@ -13,5 +14,3 @@ describe('Protractor Demo App', function() {
   });
 });
 
-// locators are used by protractor to grab elements on the page
-// for instance element(by.model('first')).sendKeys(1);
