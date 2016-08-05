8809b98fb9fe53f7e08285046f9ef6eb82d14a26
Lenny Peters <mr.lennypeters@gmail.com>
2016-08-04 20:59:54 -0700

    Master - bundle command instead of same namespace for injection

--- gulpfile.js
+++ gulpfile.js
@@ -82,6 +82,7 @@ function bundle() {
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
        // Add transformation tasks to the pipeline here.
     .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(rename({suffix: '.bundled'}))
     .pipe(gulp.dest('src/js'))
     .pipe(gulp.dest('dist/js'));
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+//required files get sorted in here
+
 var angular = require('angular');
 var uirouter = require('angular-ui-router');
 var ngstorage = require('ngstorage');
