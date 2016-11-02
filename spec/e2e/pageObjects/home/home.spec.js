3c0d38067875e218dc23f2cf7c8626c9e07b5453
Lenny Peters <mr.lennypeters@gmail.com>
2016-11-01 22:00:23 -0700

    Master - mock test to check angular

--- spec/e2e/pageObjects/home/home.spec.js
+++ spec/e2e/pageObjects/home/home.spec.js
@@ -1,8 +1,18 @@
 //test for protractor to see if its working
-describe('truthy test', function () {
-    it('should be truthy', function () {
-        expect(true).toBe(true);
-    });
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+    browser.get('https://angularjs.org');
+
+    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    element(by.css('[value="add"]')).click();
+
+    var todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // You wrote your first test, cross it off the list
+    todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
+  });
 });
-
-
